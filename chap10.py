29ae004a4f0675c63f59787116e3cb3f7b0de358
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-17 23:13:32 -0400

    Add ex.10.7

---
+++ chap10.py
@@ -0,0 +1,29 @@
+#This is where the answers to Chapter 10 questions for the BSS     Dev RampUp go
+# Name: Elizabeth Tenorio
+
+ #Ex. 10.7
+print "This program takes two strings and returns True if they are anagrams"
+
+# Sort the letters in a given word
+def sort_string(word):
+    new_word = list(word)
+    new_word.sort()
+    new_word = ''.join(new_word)
+    return new_word
+
+# Compare the sorted words to check for anagrams
+def is_anagram(first, second):
+    sortf = sort_string(first.lower())
+    sorts = sort_string(second.lower())
+    if sortf == sorts:
+        return True
+    else:
+        return False
+
+print is_anagram('star', 'rats')
+print is_anagram('dog', 'cat')
+print is_anagram('Star', 'Rats')
+
+#Ex. 10.13
+
+
---
+++ chap11.py
@@ -0,0 +1,10 @@
+#This is where the answers to Chapter 11 questions for the BSS         Dev RampUp go
+# Name: Elizabeth Tenorio
+
+#Ex. 11.9
+#Use a dictionary to write a faster, simpler version of has_duplicates.
+
+
+
+#Ex. 11.10
+#Write a program that reads a wordlist and finds all the rotate pairs.
